681f940fffc5eeab065a46a3100aea5d0c49d78a
rebeca018 <rebecasnascimento018@gmail.com>
2023-11-28 09:48:35 -0400

    Commit footer

---
+++ src/Componentes/Footer.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <div class="footer">
+        <p class="color">&copy; 2023 My Library</p>
+    </div>
+  );
+}
