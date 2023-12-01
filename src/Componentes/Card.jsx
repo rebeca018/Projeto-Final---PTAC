d76862d9a6119285a0f0dfbd533be5514441772f
rebeca018 <rebecasnascimento018@gmail.com>
2023-12-01 14:30:09 -0400

    Colocando iframe para tentar mostrar o video

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,7 +1,11 @@
 export default function Card({musica}){
     return(
         <div class="card">
-            
+            <iframe 
+                width="853"
+                height="480"
+                src='https://www.youtube.com/embed/' 
+                frameborder="0"></iframe>
             <h5 class="card-title">{musica.titulo}</h5>
             <h6 class="card-subtitle mb-2 text-body-secondary">{musica.letra}</h6>
    
