d73f993c2d11ee74d693cb2f4f007e2c35e618a3
rebeca018 <rebecasnascimento018@gmail.com>
2023-12-05 11:00:21 -0400

    Commit apagando coisa desnecessária

--- src/cadastro.jsx
+++ src/cadastro.jsx
@@ -76,16 +76,6 @@ export default function Cadastro(){
               
             </form>
 
-            {video.map((ativ)=>
-            <ul key={ativ.id}>
-                <div class="video-card">
-                    <iframe src={'https://www.youtube.com/embed/' + ativ.link.slice(17) } frameborder="0"></iframe>
-                <p class="video-card-p">{ativ.titulo}</p>
-                <p class="video-card-p">{ativ.artista}</p>
-                <p class="video-card-p">{ativ.letra}</p>
-                <p class="video-card-p">{ativ.vizualizacao}</p>
-                </div>
-            </ul>)}
             <Footer/>
         </div>
        
