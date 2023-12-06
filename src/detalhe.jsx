8fec1043433f8c757ce04c589b8cc2eaea6c7b15
rebeca018 <rebecasnascimento018@gmail.com>
2023-12-06 09:26:33 -0400

    Commit tentando fazer detalhe

--- src/detalhe.jsx
+++ src/detalhe.jsx
@@ -1,20 +1,32 @@
+import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom";
 import Header from './Componentes/Header'
 import Footer from './Componentes/Footer'
 import Card from './Componentes/Card'
+
 export default function Detalhe() {
-  const lista = JSON.parse(localStorage.getItem("Video")) || [];
+  const { id } = useParams();
+  const lista = JSON.parse(localStorage.getItem("Video"));
+  console.log(lista);
+
+  const musica = lista.filter((objeto) => {
+    if(objeto.id == id){
+       return objeto;
+    }
+    return null;
+  })
+
+  console.log(muscia[0]);
 
   return (
     <div class="home">
         <Header/>
 
-        {lista.map((musica)=>
-         <Card musica={musica}/>
-        )}
-
+        <Card musica={musica[0]} />
       <h3 class="texto-home">Detalhe</h3>
       <Footer/>
     </div>
   );
 }
+
+//import { useParams } from "react-router-dom"
